dde0cb7014c6f12ee08e818234a67e733e13247e
Kangyun Wang <me@kyunwang.nl>
2020-12-01 15:32:28 +0100

    Squashed commit of the following:
    commit 211e45e7afbe1b94c77494bbed46d80f79f2a382
    Author: Kangyun Wang <me@kyunwang.nl>
    Date:   Sun Oct 20 14:02:33 2019 +0200
    
        Remove styledcomponents
    
    commit 330c0ef23e58b105c75b6c7f85fe06d1f8156612
    Author: Kangyun Wang <me@kyunwang.nl>
    Date:   Sun Oct 20 13:58:12 2019 +0200
    
        Theme.js to postcs - add import&env config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,26 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const postCssImport = require('postcss-import');
+const postCssEnv = require('postcss-preset-env');
+const postCssCustomProperties = require('postcss-custom-properties');
+
 module.exports = {
   /* Your site config here */
   siteMetadata: { title: 'A gatsby starter' },
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        postCssPlugins: [
+          postCssEnv({
+            stage: 0,
+          }),
+          postCssImport(),
+          postCssCustomProperties(),
+        ],
+      },
+    },
+  ],
 };
--- src/components/layout/SEO.js
+++ src/components/layout/SEO.js
@@ -4,12 +4,14 @@ import { Helmet } from 'react-helmet';
 
 // WIP
 
-const SEO = ({ title, description }) => (
-  <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-  </Helmet>
-);
+const SEO = ({ title, description }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Helmet>
+  );
+};
 
 SEO.propTypes = {
   title: PropTypes.string,
